e874d570692a05a28a496e6fb79f26893fc83081
Mark Iannucci <mark.iannucci@gmail.com>
2023-01-30 15:52:21 +0000

    switch to azurerm state so we get consistent workspace support

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,8 @@
 terraform {
-  backend "remote" {
-    organization = "markiannucci"
-
-    workspaces {
-      prefix = "headscale-on-fly"
-    }
+  backend "azurerm" {
+    resource_group_name  = "headscale-tfstate"
+    storage_account_name = "tfstate20828"
+    container_name       = "tfstate"
+    key                  = "tfstate"
   }
 }
