fc5fdf86239a32b3ca39569bb74341e4ecc39240
Mark Iannucci <mark.iannucci@gmail.com>
2023-01-30 11:42:40 -0700

    Confirm TF workspaces work (#12)
    * empty space change
    
    * fix backend badness?
    
    * use cloud backend again
    
    * use backend remote correctly
    
    * switch to azurerm state so we get consistent workspace support
    
    * add ARM_ACCESS_KEY secret
    
    * need $
    
    * use  for tf workspaces
    
    * add debug workspace list
    
    * don't use terraform wrapper and see if this fixes our workspace problems
    
    * fix terraform workspace issue
    
    * use exact match with grep
    
    * update to primary region
    
    * fix grep fail
    
    * add PRIMARY_REGION to fly.toml
    
    * use end of string delimiter

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,8 @@
 terraform {
-  cloud {
-    organization = "markiannucci"
-
-    workspaces {
-      name = "headscale-on-fly"
-    }
+  backend "azurerm" {
+    resource_group_name  = "headscale-tfstate"
+    storage_account_name = "tfstate20828"
+    container_name       = "tfstate"
+    key                  = "tfstate"
   }
-
 }
